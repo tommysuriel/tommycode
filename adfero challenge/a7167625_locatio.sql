51a706a771f2762c4cdc8d946064c3a839507bc0
Tommy Suriel <tommy.suriel@udc.edu>
2015-01-24 19:47:58 -0500

    sdfgsdfg
    sdfgsdfg

--- adfero challenge/a7167625_locatio.sql
+++ adfero challenge/a7167625_locatio.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `locations`
+-- Database: `a7167625_locatio`
 --
 
 -- --------------------------------------------------------
